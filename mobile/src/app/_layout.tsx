2dabe87da779e29e6aa8572e2f066005a4a29e02
aravinth2760 <aravinth2760@gmail.com>
2026-07-01 08:55:52 +0530

    Add Redux Toolkit setup

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import ReduxProvider from "@/redux/provider";
+
+import { store } from "../redux/store";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ReduxProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </ReduxProvider>
+  );
 }
