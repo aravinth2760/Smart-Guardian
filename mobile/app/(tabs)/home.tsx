8c04c19de6b34c98245a3c95ed8ea77ce71addfd
aravinth2760 <aravinth2760@gmail.com>
2026-07-04 14:14:27 +0530

    feat: add HomeSOSCard to home screen

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,24 +1,31 @@
-import { ScrollView, StatusBar } from "react-native";
+import { Alert, ScrollView, StatusBar } from "react-native";
 import { router } from "expo-router";
 import { useDispatch } from "react-redux";
 
 import { logout } from "@/store/slices/authSlice";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import HomeHeader from "@/components/home/HomeHeader";
+import HomeSOSCard from "@/components/home/HomeSOSCard";
 
 export default function HomeScreen() {
   const dispatch = useDispatch();
   const [contact, setContact] = useState(0);
+  const [sendingAlert, setSendingAlert] = useState(false);
 
   const handleLogout = () => {
     dispatch(logout());
     router.replace("/(auth)/login");
   };
 
+  const handleSOSPress = useCallback(() => {
+    Alert.alert("🚨 SOS Button", "Emergency button clicked!");
+  }, []);
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <StatusBar barStyle="default" />
       <HomeHeader contacts={contact} />
+      <HomeSOSCard onPress={handleSOSPress} disabled={sendingAlert} />
     </ScrollView>
   );
 }
