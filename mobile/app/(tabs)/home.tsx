b2086bee34c3eab5f33abed9516f3f00ff1eaea6
aravinth2760 <aravinth2760@gmail.com>
2026-07-07 13:16:59 +0530

    refactor: remove unused dispatch and logout import from Home.tsx

--- mobile/app/(tabs)/home.tsx
+++ mobile/app/(tabs)/home.tsx
@@ -1,16 +1,13 @@
 import { Alert, ScrollView, StatusBar } from "react-native";
 import { router } from "expo-router";
-import { useDispatch } from "react-redux";
-
-import { logout } from "@/store/slices/authSlice";
 import { useCallback, useState } from "react";
+
 import HomeHeader from "@/components/home/HomeHeader";
 import HomeSOSCard from "@/components/home/HomeSOSCard";
 import HomeQuickActions from "@/components/home/HomeQuickActions";
 import GuardiansList from "@/components/home/HomeGuardians";
 
 export default function HomeScreen() {
-  const dispatch = useDispatch();
   const [contact, setContact] = useState(0);
   const [sendingAlert, setSendingAlert] = useState(false);
 
