e504e959dc819c2d0d47b731b1b7c68786879c37
aravinth2760 <aravinth2760@gmail.com>
2026-07-08 13:59:26 +0530

    add update profile service

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -3,29 +3,40 @@ import { prisma } from "../prisma/client.js";
 
 interface CompleteProfileDto {
   name: string;
-  email?: string;
+  email: string;
   relationship: Relationship;
 }
+
+interface UpdateProfileDto {
+  name: string;
+  email: string;
+  relationship: Relationship;
+}
+
+const validateEmail = async (userId: string, email: string) => {
+  const normalizedEmail = email.trim().toLowerCase();
+
+  const existingEmail = await prisma.user.findFirst({
+    where: {
+      email: normalizedEmail,
+      NOT: {
+        id: userId,
+      },
+    },
+  });
+
+  if (existingEmail) {
+    throw new Error("Email already exists");
+  }
+
+  return normalizedEmail;
+};
 
 export const completeProfile = async (
   userId: string,
   data: CompleteProfileDto,
 ) => {
-  // Check email already exists
-  if (data.email) {
-    const existingEmail = await prisma.user.findFirst({
-      where: {
-        email: data.email,
-        NOT: {
-          id: userId,
-        },
-      },
-    });
-
-    if (existingEmail) {
-      throw new Error("Email already exists");
-    }
-  }
+  const email = await validateEmail(userId, data.email);
 
   const user = await prisma.user.update({
     where: {
@@ -33,19 +44,49 @@ export const completeProfile = async (
     },
     data: {
       name: data.name.trim(),
-      email: data.email?.trim().toLowerCase(),
+      email,
       relationship: data.relationship,
       profileCompleted: true,
     },
     select: {
       id: true,
+      familyId: true,
       phone: true,
       name: true,
       email: true,
       relationship: true,
       role: true,
       profileCompleted: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return user;
+};
+
+export const updateProfile = async (userId: string, data: UpdateProfileDto) => {
+  console.log(userId, data);
+  const email = await validateEmail(userId, data.email);
+
+  const user = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      name: data.name.trim(),
+      email,
+      relationship: data.relationship,
+    },
+    select: {
+      id: true,
       familyId: true,
+      phone: true,
+      name: true,
+      email: true,
+      relationship: true,
+      role: true,
+      profileCompleted: true,
       createdAt: true,
       updatedAt: true,
     },
