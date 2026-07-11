cb5408db808ab8a3b8ba2c20582a1b869a3032f6
aravinth2760 <aravinth2760@gmail.com>
2026-07-11 10:56:05 +0530

    fix: add global socket connection handling

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -6,8 +6,21 @@ import { store } from "@/store/index";
 import AuthProvider from "@/provider/AuthProvider";
 import { ContactsProvider } from "@/provider/ContactsProvider";
 import { SocketProvider } from "@/provider/SocketProvider";
+import { useEffect } from "react";
+import { socket } from "@/services/socket";
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (!socket.connected) {
+      socket.connect();
+    }
+    return () => {
+      if (socket.connected) {
+        socket.disconnect();
+      }
+    };
+  }, []);
+
   return (
     <Provider store={store}>
       <AuthProvider>
