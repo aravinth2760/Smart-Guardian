35c74f1cc640471d805f630ae23262bfe48fcb32
aravinth2760 <aravinth2760@gmail.com>
2026-07-12 16:44:33 +0530

    refactor: move SOS settings screen to profile/sos

--- mobile/src/store/index.ts
+++ mobile/src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "@/store/slices/authSlice";
+import sosSettingsReducer from "@/store/slices/sosSettingsSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    sosSettings: sosSettingsReducer,
   },
 });
 
